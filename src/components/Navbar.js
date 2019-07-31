fedd640914b6b262457a3864e01fb67d241cca3c
Matt Westover <matthew.westover@gmail.com>
2019-07-30 23:31:36 -0600

    update navbar and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
-import { Navbar as Nav } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import Logo from '../assets/PokemonLogo.png'
 
-class Navbar extends Component {
+class NavStuff extends Component {
   render() {
     return(
-      <Nav bg='dark' fixed='top' variant='dark'>
-        <Nav.Brand href='/'>Pokedex</Nav.Brand>
-      </Nav>
+      <Navbar bg='red' fixed='top' variant='dark'>
+        <Navbar.Brand href="/" className="pl-5">
+          <img
+            src={Logo}
+            alt="Pokemon"
+            height="40px"
+          />
+        </Navbar.Brand>
+        <Nav className="mr-auto" style={{ color: 'white !important'}}>
+          <Nav.Link href="/">Pokédex</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Coming Soon!" className="mr-sm-2" disabled />
+          <Button variant="primary" disabled>Search</Button>
+        </Form>
+      </Navbar>
     )
   }
 }
 
-export default Navbar;
+export default NavStuff;
