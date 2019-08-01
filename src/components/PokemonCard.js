221b5fa195b9b065573f94047ccbea9245c7dbcc
Matt Westover <matthew.westover@gmail.com>
2019-08-01 14:56:51 -0600

    set initial api call for card usage

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import { Card } from 'react-bootstrap';
 
 class PokemonCard extends Component {
+  state = {
+    name: '',
+    imageURL: '',
+    pokemonIndex: '',
+  }
+
+  componentDidMount() {
+    const {name, url} = this.props;
+    const pokemonIndex = url.split('/')[url.split('/').length - 2]
+    const imageURL = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+
+    this.setState({name, imageURL, pokemonIndex})
+  }
+
   render() {
+    const {name, imageURL, pokemonIndex} = this.state;
+
     return(
       <Card text='light' className='cardStyle col-md-2 col-sm-12' style={{ marginBottom: '1rem'}}>
-        <Card.Header>Test Header</Card.Header>
+        <Card.Header>{name}</Card.Header>
         <Card.Body>
           <Card.Text>Test Card</Card.Text>
         </Card.Body>
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
 import PokemonCard from './PokemonCard';
 
 class PokemonList extends Component {
+  state = {
+    url: 'https://pokeapi.co/api/v2/pokemon',
+    pokemon: null,
+  }
+
+  // ?limit=807&offset=0
+
+  async componentDidMount() {
+    await axios.get(this.state.url)
+      .then( res => {
+        this.setState({ pokemon: res.data.results})
+      })
+  }
+
   render() {
     return(
-      <div className='row' style={{ justifyContent: 'space-between' }}>
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-      </div>
+      <Fragment>
+        {this.state.pokemon ? (
+          <div className='row' style={{ justifyContent: 'space-between' }}>
+            {this.state.pokemon.map( pokemon => (
+              <PokemonCard key= {pokemon.name} name={pokemon.name} url={pokemon.url} />
+              ))}
+          </div>
+        ) : (
+          <h1>Loading Pokémon</h1>
+        )}
+      </Fragment>
     )
   }
 };
